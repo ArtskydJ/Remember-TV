9d0459527de7dd8be0d82c34c71348bd198b7010
Joseph Dykstra <josephdykstra@gmail.com>
2019-01-29 22:26:14 -0600

    Use a good icon from pixabay

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function createWindow () {
 	mainWindow = new BrowserWindow({
 		width: 800,
 		height: 600,
-		icon: './icon/attempt1_64.png'
+		icon: './icon/icon-2141485_32.png'
 	})
 
 	// and load the index.html of the app.
